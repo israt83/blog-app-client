0c9c76ec6a39eeeefc45f505eb8bdee8bd69545b
Israt Jahan Jisha <isratjahanfci@gmail.com>
2026-02-06 20:29:15 +0600

    initial commit

---
+++ src/app/about/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const aboutPage = () => {
+  return (
+    <div>about Page</div>
+  )
+}
+
+export default aboutPage
---
+++ src/app/contact/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const pageContact = () => {
+  return (
+    <div>page Contact</div>
+  )
+}
+
+export default pageContact
